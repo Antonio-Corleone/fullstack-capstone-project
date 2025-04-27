f7f07609671abe2689c127ad69a4835aab0e115e
Antonio-Corleone <vuminhtuan3795@gmail.com>
2025-04-26 21:34:51 -0400

    feat: implement register route

---
+++ giftlink-backend/routes/authRoutes.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { body, validationResult } = require('express-validator');
+const connectToDatabase = require('../models/db');
+const router = express.Router();
+const dotenv = require('dotenv');
+const pino = require('pino');
+
+const logger = pino();
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+router.post('/register', async (req, res) => {
+    const { email, firstName, lastName, password } = req.body;
+    try {
+        const db = await connectToDatabase();
+
+        const collection = db.collection("users");
+        const existingEmail = await collection.findOne({ email });
+        if (existingEmail) {
+            return res.status(400).send('User have already existed!')
+        }
+
+        const salt = await bcryptjs.genSalt(10);
+        const hash = await bcryptjs.hash(password, salt);
+
+        const newUser = await collection.insertOne({
+            email,
+            firstName,
+            lastName,
+            password: hash,
+            createdAt: new Date(),
+        });
+
+        const payload = {
+            user: {
+                id: newUser.insertedId,
+            },
+        };
+
+        const authtoken = jwt.sign(payload, JWT_SECRET);
+
+        logger.info('User registered successfully');
+        res.json({ authtoken, email });
+    } catch (e) {
+        return res.status(500).send('Internal server error');
+    }
+});
