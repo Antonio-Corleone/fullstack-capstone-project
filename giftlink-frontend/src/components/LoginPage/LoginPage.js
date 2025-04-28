395f475b3e42bbd6868486fe0c123299ce502d74
Antonio-Corleone <vuminhtuan3795@gmail.com>
2025-04-28 05:34:22 -0400

    feat: implement handleLogin function

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,13 +1,57 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { urlConfig } from '../../config';
+import { useAppContext } from '../../context/AuthContext';
 import './LoginPage.css';
 
 function LoginPage() {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [incorrect, setIncorrect] = useState('');
+
+    const navigate = useNavigate();
+    const bearerToken = sessionStorage.getItem('bearer-token');
+    const { setIsLoggedIn } = useAppContext();
+
+    useEffect(() => {
+        if (sessionStorage.getItem('auth-token')) {
+            navigate('/app')
+        }
+    }, [navigate])
 
     const handleLogin = async () => {
-        console.log("Inside handleLogin");
+        e.preventDefault();
+        //api call
+        const res = await fetch(`${urlConfig.backendUrl}/api/auth/login`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': bearerToken ? `Bearer ${bearerToken}` : '',
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password,
+            })
+        });
+
+        const json = await res.json();
+        console.log('Json', json);
+        if (json.authtoken) {
+            sessionStorage.setItem('auth-token', json.authtoken);
+            sessionStorage.setItem('name', json.userName);
+            sessionStorage.setItem('email', json.userEmail);
+            setIsLoggedIn(true);
+            navigate('/app');
+        } else {
+            document.getElementById("email").value = "";
+            document.getElementById("password").value = "";
+            setIncorrect("Wrong email or password. Try again.");
+            setTimeout(() => {
+                setIncorrect("");
+            }, 2000);
+        }
+
     }
 
     return (
@@ -39,7 +83,7 @@ function LoginPage() {
                                 onChange={(e) => setPassword(e.target.value)}
                             />
                         </div>
-                        {/* Include appropriate error message if login is incorrect*/}
+                        <span style={{ color: 'red', height: '.5cm', display: 'block', fontStyle: 'italic', fontSize: '12px' }}>{incorrect}</span>
                         <button className="btn btn-primary w-100 mb-3" onClick={handleLogin}>Login</button>
 
                         <p className="mt-4 text-center">
